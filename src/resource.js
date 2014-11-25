6c6f1ee5df7baf2278cfb5d374405638837b89c7
janniksu <mail@janniksu.de>
2014-11-25 14:18:24 +0100

    all

--- src/resource.js
+++ src/resource.js
@@ -1,7 +1,9 @@
 var res = {
     HelloWorld_png : "res/HelloWorld.png",
     CloseNormal_png : "res/CloseNormal.png",
-    CloseSelected_png : "res/CloseSelected.png"
+    CloseSelected_png : "res/CloseSelected.png",
+    background1_png : "res/background1.png",
+    zombie1_png : "res/zombie1.png"
 };
 
 var g_resources = [];
